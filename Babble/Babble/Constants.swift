53f3487f5c0a2ab54111e44577bf2c3bcbd20b20
iOSMoonSand <alexisschreier@gmail.com>
2016-06-30 20:16:15 -0700

    Add FirebaseAuth and FirebaseDatabase pods, create DataService service class. Stuck on Firebase import error.

---
+++ Babble/Babble/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Babble
+//
+//  Created by Alexis Schreier on 06/30/16.
+//  Copyright © 2016 Alexis Schreier. All rights reserved.
+//
+
+
+import Foundation
+
+let BASE_URL = "https://babble-8b668.firebaseio.com/"
--- Babble/Babble/DataService.swift
+++ Babble/Babble/DataService.swift
@@ -5,5 +5,38 @@
 //  Created by Alexis Schreier on 06/30/16.
 //  Copyright © 2016 Alexis Schreier. All rights reserved.
 //
+//This is a service class that interacts with Firebase
+ //define singleton shared instance and instantiate
+//create Firebase database reference with the Firebase URL to read and write data
 
 import Foundation
+import FirebaseAuth
+import FirebaseDatabase
+
+class DataService {
+    static let dataService = DataService()
+    
+    private var _BASE_REF = Firebase(url: "\(BASE_URL)")
+    private var _USER_REF = Firebase(url: "\(BASE_URL)/users")
+    private var _JOKE_REF = Firebase(url: "\(BASE_URL)/jokes")
+    
+    var BASE_REF: Firebase {
+        return _BASE_REF
+    }
+    
+    var USER_REF: Firebase {
+        return _USER_REF
+    }
+    
+    var CURRENT_USER_REF: Firebase {
+        let userID = NSUserDefaults.standardUserDefaults().valueForKey("uid") as! String
+        
+        let currentUser = Firebase(url: "\(BASE_REF)").childByAppendingPath("users").childByAppendingPath(userID)
+        
+        return currentUser!
+    }
+    
+    var JOKE_REF: Firebase {
+        return _JOKE_REF
+    }
+}
