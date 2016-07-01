2598f849f6b1748413a7828e20c276ffbe29314a
iOSMoonSand <alexisschreier@gmail.com>
2016-06-30 18:00:58 -0700

    Create view controller files for login and account creation, create UI for login and account creation, link up outlets and actions from storyboard to code.

---
+++ Babble/Babble/CreateAccountViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CreateAccountViewController.swift
+//  Babble
+//
+//  Created by Alexis Schreier on 06/30/16.
+//  Copyright © 2016 Alexis Schreier. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBAction func createAccountAction(sender: AnyObject) {
+    }
+    
+    
+    @IBAction func cancelAction(sender: AnyObject) {
+    }
+}
---
+++ Babble/Babble/DataService.swift
@@ -0,0 +1,9 @@
+//
+//  DataService.swift
+//  Babble
+//
+//  Created by Alexis Schreier on 06/30/16.
+//  Copyright © 2016 Alexis Schreier. All rights reserved.
+//
+
+import Foundation
