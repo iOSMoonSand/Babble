f0bbeeb16d5a46b30a85b372fce94db262e0d6b6
iOSMoonSand <alexisschreier@gmail.com>
2016-07-02 10:50:12 -0700

    Edit comments

--- Babble/Babble/DataService.swift
+++ Babble/Babble/DataService.swift
@@ -6,40 +6,22 @@
 //  Copyright © 2016 Alexis Schreier. All rights reserved.
 //
 //This is a service class that interacts with Firebase
- //define singleton shared instance and instantiate
-//create Firebase database reference with the Firebase URL to read and write data
 
 import Foundation
 import FirebaseAuth
 import FirebaseDatabase
-import FirebaseAuthUI
-import FirebaseDatabaseUI
 
 class DataService {
     
-    static let dataService = DataService()
+    //singleton shared instance
+    static let sharedManager = DataService()
     
-    private var _BASE_REF = Firebase(url: "\(BASE_URL)")
-    private var _USER_REF = Firebase(url: "\(BASE_URL)/users")
-    private var _JOKE_REF = Firebase(url: "\(BASE_URL)/jokes")
+    //create a reference to your database
+    let firebaseRef = FIRDatabase.database().reference()
     
-    var BASE_REF: Firebase {
-        return _BASE_REF
-    }
-    
-    var USER_REF: Firebase {
-        return _USER_REF
-    }
-    
-    var CURRENT_USER_REF: Firebase {
-        let userID = NSUserDefaults.standardUserDefaults().valueForKey("uid") as! String
+    func funky{
         
-        let currentUser = Firebase(url: "\(BASE_REF)").childByAppendingPath("users").childByAppendingPath(userID)
-        
-        return currentUser!
+        firebaseRef.ch
     }
     
-    var JOKE_REF: Firebase {
-        return _JOKE_REF
-    }
 }
