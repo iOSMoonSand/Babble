9110c37a5f48ce78fefe43b426598bb891989c7a
iOSMoonSand <alexisschreier@gmail.com>
2016-07-26 23:32:11 -0700

    After a valliant battle, Firebase has been conquered: modify performSegueWithIdentifier method and add prepareForSegue method in HomeScreenViewController, add to AnswersViewController to pull Answer data from Firebase.

--- Babble/Babble/AnswersViewController.swift
+++ Babble/Babble/AnswersViewController.swift
@@ -8,61 +8,45 @@
 
 import UIKit
 import Firebase
-    // MARK: -
-    // MARK: - AnswersViewController Class
-    // MARK: -
+
 class AnswersViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    // MARK: -
-    // MARK: - Properties
-    // MARK: -
+
+// MARK: - Properties
     @IBOutlet weak var tableView: UITableView!
     var ref: FIRDatabaseReference!
     private var _refHandle: FIRDatabaseHandle!
     var answersArray: [FIRDataSnapshot]! = [] //empty array that can hold data snapshots of answers
     var questionRef: String?
-    // MARK: -
-    // MARK: - Init Methods
-    // MARK: -
-    deinit {
-//            self.ref.child("answers").child(questionRef).removeObserverWithHandle(_refHandle)
-    }
-    // MARK: -
-    // MARK: - UIViewController Methods
-    // MARK: -
+
+    
+// MARK: - UIViewController Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         self.ref = FIRDatabase.database().reference()
         self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "tableViewCell")
         self.configureDatabase()
     }
-    // MARK: -
-    // MARK: - Firebase Database Configuration
-    // MARK: -
+    
+    deinit {
+        self.ref.child("answers").child(questionRef!).removeObserverWithHandle(_refHandle)
+    }
+
+    
+// MARK: - Firebase Database Configuration
     func configureDatabase() {
         self.ref = FIRDatabase.database().reference()
-        //listen for new answers for the selected question
-//        let answerRef = self.ref.child("answers")
-        if let tempQuestionRef = questionRef {
-            self.ref.child("answers").child(tempQuestionRef).observeSingleEventOfType(.Value, withBlock: { snapshot in
-                for child in snapshot.children {
-                    let key = child.key as String
-                    let name = child.value["name"] as! String
-                    let text = child.value["text"] as! String
-                    print("answer: \(key)  name: \(name) text: \(text)")
-                    
-                    self.answersArray.append(child as! FIRDataSnapshot)
-                    self.tableView.insertRowsAtIndexPaths([NSIndexPath(forRow: self.answersArray.count-1, inSection: 0)], withRowAnimation: .Automatic)
-                }
-                //reload the tableview
-            })
-        }
+        _refHandle = self.ref.child("answers").child(questionRef!).observeEventType(.ChildAdded, withBlock: { (snapshot) -> Void in
+            self.answersArray.append(snapshot)
+            self.tableView.insertRowsAtIndexPaths([NSIndexPath(forRow: self.answersArray.count-1, inSection: 0)], withRowAnimation: .Automatic)
+        })
     }
-    // MARK: -
-    // MARK: - UITableViewDataSource & UITableViewDelegate methods
-    // MARK: -
+    
+    
+// MARK: - UITableViewDataSource & UITableViewDelegate methods
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.answersArray.count
     }
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell: UITableViewCell! = self.tableView.dequeueReusableCellWithIdentifier("tableViewCell", forIndexPath: indexPath)
         //unpack question from database
--- Babble/Babble/HomeScreenViewController.swift
+++ Babble/Babble/HomeScreenViewController.swift
@@ -11,90 +11,70 @@ import Firebase
 
 class HomeScreenViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
 
-// MARK: - Instance Variables
+// MARK: - Properties
+    @IBOutlet weak var tableView: UITableView!
     var ref: FIRDatabaseReference!
     private var _refHandle: FIRDatabaseHandle!
     var questionsArray: [FIRDataSnapshot]! = [] //empty array that can hold data snapshots of questions
-    
-    
-// MARK: - IBOutlets
-    @IBOutlet weak var tableView: UITableView!
-    
 
-// MARK: - View Loading & Appearing
+
+// MARK: - UIViewController Methods
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        
         ref = FIRDatabase.database().reference()
         self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "tableViewCell")
-        
         configureDatabase()
     }
     
-    override func viewWillDisappear(animated: Bool) {
-        
-        self.ref.removeObserverWithHandle(_refHandle)
+    deinit {
+        self.ref.child("questions").removeObserverWithHandle(_refHandle)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == Constants.Segues.HomeToAnswersNavController {
+            guard let selectedIndexPath = self.tableView.indexPathForSelectedRow else { return }
+            let questionSnapShot: FIRDataSnapshot! = self.questionsArray[selectedIndexPath.row]
+            let selectedQuestion = questionSnapShot.key
+            
+            guard let destinationVC = segue.destinationViewController as? AnswersViewController else { return }
+            destinationVC.questionRef = selectedQuestion
+        }
     }
 
+    
 // MARK: - Firebase Database Configuration
     func configureDatabase() {
-        
         ref = FIRDatabase.database().reference()
-        
-        //listen for new questions in the database
         _refHandle = self.ref.child("questions").observeEventType(.ChildAdded, withBlock: {(snapshot) -> Void in
-            
             self.questionsArray.append(snapshot)
             self.tableView.insertRowsAtIndexPaths([NSIndexPath(forRow: self.questionsArray.count-1, inSection: 0)], withRowAnimation: .Automatic)
         })
     }
     
-    deinit {
-        
-        self.ref.child("questions").removeObserverWithHandle(_refHandle)
-    }
-    
     
 // MARK: - UITableViewDataSource & UITableViewDelegate methods
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
         return questionsArray.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
         let cell: UITableViewCell! = self.tableView.dequeueReusableCellWithIdentifier("tableViewCell", forIndexPath: indexPath)
-        
         //unpack question from database
         let questionSnapshot: FIRDataSnapshot! = self.questionsArray[indexPath.row]
         let question = questionSnapshot.value as! Dictionary<String, String>
         let name = question[Constants.QuestionFields.name] as String!
         let text = question[Constants.QuestionFields.text] as String!
-        
+        //assign data to cell
         cell!.textLabel?.text = name + ": " + text
         cell!.imageView?.image = UIImage(named: "ic_account_circle")
         if let photoUrl = question[Constants.QuestionFields.photoUrl], url = NSURL(string:photoUrl), data = NSData(contentsOfURL: url) {
         cell!.imageView?.image = UIImage(data: data)
         }
-        
         return cell!
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        
-        let questionSnapshot: FIRDataSnapshot! = self.questionsArray[indexPath.row]
-        let selectedQuestion = questionSnapshot.key
-        
-        let destinationVC = AnswersViewController()
-        let answerVCQuestionRef = destinationVC.questionRef
-        if var tempAnswerVCQuestionRef = answerVCQuestionRef {
-            tempAnswerVCQuestionRef = selectedQuestion
-            print("User tapped on: \(answerVCQuestionRef)")
-        }
-        
         performSegueWithIdentifier(Constants.Segues.HomeToAnswersNavController, sender: self)
-        
     }
     
     
