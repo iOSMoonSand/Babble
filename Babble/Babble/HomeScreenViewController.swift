3a501236c25116baa71af9d3b308acfe8cb6aaa7
iOSMoonSand <alexisschreier@gmail.com>
2016-08-16 22:31:20 -0700

    Saving progress

--- Babble/Babble/HomeScreenViewController.swift
+++ Babble/Babble/HomeScreenViewController.swift
@@ -45,12 +45,9 @@ class HomeScreenViewController: UIViewController, UITableViewDelegate, UITableVi
             guard let selectedIndexPath = self.tableView.indexPathForSelectedRow else { return }
             let questionSnapShot = self.questionsArray[selectedIndexPath.row]
             let questionID = questionSnapShot[Constants.QuestionFields.questionID]
-            
             guard let nav = segue.destinationViewController as? UINavigationController else { return }
             guard let answersVC = nav.topViewController as? AnswersViewController else { return }
-            
             answersVC.questionRef = questionID as? String
-            
             guard let destinationVC = segue.destinationViewController as? AnswersViewController else { return }
             destinationVC.questionRef = questionID as? String
         }
@@ -60,15 +57,18 @@ class HomeScreenViewController: UIViewController, UITableViewDelegate, UITableVi
     // MARK:
     func configureDatabase() {
         ref = FIRDatabase.database().reference()
-        _refHandle = self.ref.child("questions").observeEventType(.ChildAdded, withBlock: {[weak self] (questionSnapshot) in
+        _refHandle = self.ref.child("questions").observeEventType(.ChildAdded, withBlock: {/*[weak self]*/ (questionSnapshot) in
             let questionID = questionSnapshot.key
             var question = questionSnapshot.value as! [String: AnyObject]
             question[Constants.QuestionFields.questionID] = questionID
             let userID = question[Constants.QuestionFields.userID] as! String
             //let likeCount = question[Constants.QuestionFields.likeCount] as! Int
             
-            let usersRef = self?.ref.child("users")
-            usersRef?.child(userID).observeEventType(.Value, withBlock: { (userSnapshot) in
+            let usersRef = self.ref.child("users")
+            //
+            //what if we change this to .ChildAdded so that
+            //
+            usersRef.child(userID).observeEventType(.Value, withBlock: { (userSnapshot) in
                 var user = userSnapshot.value as! [String: AnyObject]
                 let photoURL = user[Constants.UserFields.photoUrl] as! String
                 let displayName = user[Constants.UserFields.displayName] as! String
@@ -76,26 +76,33 @@ class HomeScreenViewController: UIViewController, UITableViewDelegate, UITableVi
                 question[Constants.QuestionFields.photoUrl] = photoURL
                 question[Constants.QuestionFields.displayName] = displayName
                 
-//                var reload = false
-//                for dict in (self?.questionsArray)! {
-//                    guard let dictQuestionId = dict[Constants.QuestionFields.questionID] as? String else { continue }
-//                    guard let newQuestionId = dict[Constants.QuestionFields.questionID] as? String else { continue }
-//                    if dictQuestionId == newQuestionId {
-//                    //change photo url of dictionary
-//                    reload = true
-//                    
-//                    }
-//                    
-//                }
-//                // check to see if the newest questionId is in the questionsArray, if it is, go to that index in the questionArray and change the imageUrl
-//                // if not, append the question to the questionsArray
-//                if reload {
-//                    self?.tableView.reloadData()
-//                } else {
-                self?.questionsArray.append(question)
-                    self?.tableView.insertRowsAtIndexPaths([NSIndexPath(forRow: (self?.questionsArray.count)!-1, inSection: 0)], withRowAnimation: .Automatic)
-//                }
-                print(self!.questionsArray)
+                var reload = false
+                //guard let questionsArray = self.questionsArray else { return }
+                var i = 0
+                for (index, dict) in self.questionsArray.enumerate() {
+                    guard let dictQuestionId = dict[Constants.QuestionFields.questionID] as? String else { continue }
+                    guard let newQuestionId = question[Constants.QuestionFields.questionID] as? String else { continue }
+                    if dictQuestionId == newQuestionId {
+                    //change photo url of dictionary
+                        reload = true
+                        i = index
+                    }
+                    
+                }
+                // check to see if the newest questionId is in the questionsArray, if it is, go to that index in the questionArray and change the imageUrl
+                // if not, append the question to the questionsArray
+                if reload {
+                    var questionToChange = self.questionsArray[i]
+                    let photoURL = question[Constants.QuestionFields.photoUrl]
+                    questionToChange[Constants.QuestionFields.photoUrl] = photoURL
+                    print("Photo URL in for loop: \(photoURL)")
+                    print("QuestionToChange Photo URL : \(questionToChange[Constants.QuestionFields.photoUrl])")
+                    self.tableView.reloadData()
+                } else {
+                self.questionsArray.append(question)
+                    self.tableView.insertRowsAtIndexPaths([NSIndexPath(forRow: (self.questionsArray.count) -1, inSection: 0)], withRowAnimation: .Automatic)
+                }
+                print(self.questionsArray)
                 
             })
             { (error) in
@@ -114,7 +121,6 @@ class HomeScreenViewController: UIViewController, UITableViewDelegate, UITableVi
         let cell = self.tableView.dequeueReusableCellWithIdentifier("QuestionCell", forIndexPath: indexPath) as! QuestionCell
         
         //cell.likeButton.tag = indexPath.row
-
         //cell.likeButton.addTarget(self, action: "updateLikeButtonAndCount:", forControlEvents: .TouchUpInside)
         
         //unpack question from local dict
