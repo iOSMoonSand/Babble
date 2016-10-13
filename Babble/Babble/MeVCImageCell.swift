212cdf8eb36e2f47bbb080fd4dd7c242d1dedc4a
iOSMoonSand <alexisschreier@gmail.com>
2016-10-12 17:43:06 -0700

    Redesign MeVC to MyProfileVC. Took out table view and replaced with scroll view.

--- Babble/Babble/MeVCImageCell.swift
+++ Babble/Babble/MeVCImageCell.swift
@@ -1,33 +1,33 @@
+////
+////  MeVCImageCell.swift
+////  Babble
+////
+////  Created by Alexis Schreier on 09/13/16.
+////  Copyright © 2016 Alexis Schreier. All rights reserved.
+////
 //
-//  MeVCImageCell.swift
-//  Babble
+//import UIKit
 //
-//  Created by Alexis Schreier on 09/13/16.
-//  Copyright © 2016 Alexis Schreier. All rights reserved.
+//protocol MeVCImageCellDelegate: class {
+//    func didTapProfilePhotoImageView()
+//}
 //
-
-import UIKit
-
-protocol MeVCImageCellDelegate: class {
-    func didTapProfilePhotoImageView()
-}
-
-class MeVCImageCell: UITableViewCell {
-    @IBOutlet weak var profileImageView: UIImageView!
-    @IBOutlet weak var editLabel: UILabel!
-    weak var delegate: MeVCImageCellDelegate?
-    
-    override func awakeFromNib() {
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.imageTapped))
-        self.profileImageView.addGestureRecognizer(tapGesture)
-//        self.profileImageView.layer.borderWidth = 1
-//        self.profileImageView.layer.masksToBounds = false
-//        self.profileImageView.layer.borderColor = UIColor.blackColor().CGColor
-//        self.profileImageView.layer.cornerRadius = profileImageView.bounds.width/2
-//        self.profileImageView.clipsToBounds = true
-    }
-    
-    func imageTapped() {
-        delegate?.didTapProfilePhotoImageView()
-    }
-}
+//class MeVCImageCell: UITableViewCell {
+//    @IBOutlet weak var profileImageView: UIImageView!
+//    @IBOutlet weak var editLabel: UILabel!
+//    weak var delegate: MeVCImageCellDelegate?
+//    
+//    override func awakeFromNib() {
+//        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.imageTapped))
+//        self.profileImageView.addGestureRecognizer(tapGesture)
+////        self.profileImageView.layer.borderWidth = 1
+////        self.profileImageView.layer.masksToBounds = false
+////        self.profileImageView.layer.borderColor = UIColor.blackColor().CGColor
+////        self.profileImageView.layer.cornerRadius = profileImageView.bounds.width/2
+////        self.profileImageView.clipsToBounds = true
+//    }
+//    
+//    func imageTapped() {
+//        delegate?.didTapProfilePhotoImageView()
+//    }
+//}
